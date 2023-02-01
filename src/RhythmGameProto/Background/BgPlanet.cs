e28600cd7c44dd8a2921701aa77c8a6aeb32911a
Mack Pearson <76492881+chopov1@users.noreply.github.com>
2023-02-01 13:18:12 -0600

    added animated planet in menu BG

---
+++ src/RhythmGameProto/Background/BgPlanet.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhythmGameProto.Background
+{
+    internal class BgPlanet : Sprite
+    {
+        Texture2D[] frames;
+        enum CountState { up, down };
+        CountState state;
+        public float Transparency;
+        float fadeSpeed;
+        public Texture2D Texture { get { return spriteTexture; } }
+
+        Animation anim;
+
+        Random rnd;
+        public BgPlanet(Game game, string texturename, Camera camera) : base(game, texturename, camera)
+        {
+            Game.Components.Add(this);
+            rnd = new Random();
+            Transparency = rnd.Next(100) * 0.01f;
+            state = CountState.up;
+            fadeSpeed = 0.001f * rnd.Next(1, 5);
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            spriteTexture = Game.Content.Load<Texture2D>("bluePlanet/BluePlanetSheet");
+            anim = new Animation(spriteTexture, 6, .1f);
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+            anim.Start();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            anim.Update(gameTime);
+            if (Transparency <= 0)
+            {
+                state = CountState.up;
+                getRndPos(this);
+            }
+            if (Transparency >= 1)
+            {
+                state = CountState.down;
+            }
+            switch (state)
+            {
+                case CountState.up:
+                    Transparency += fadeSpeed;
+                    break;
+                case CountState.down:
+                    Transparency -= fadeSpeed;
+                    break;
+            }
+        }
+
+        public void DrawAnim(SpriteBatch s)
+        {
+            anim.Draw(Position, s,Transparency);
+        }
+
+        private void getRndPos(Sprite s)
+        {
+            s.Position = new Vector2(rnd.Next(Game.GraphicsDevice.Viewport.Width), rnd.Next(Game.GraphicsDevice.Viewport.Height));
+        }
+
+    }
+}
---
+++ src/RhythmGameProto/GameUtility/Animation.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhythmGameProto
+{
+    public class Animation
+    {
+
+        private Texture2D texture;
+        List<Rectangle> _sourceRectangles;
+        int frames;
+        int frame;
+        float frameTime;
+        float frameTimeLeft;
+        bool active = true;
+        public Animation(Texture2D spriteSheet, int framesX, float frameTime) 
+        {
+            _sourceRectangles = new List<Rectangle>();
+            texture= spriteSheet;
+            frames = framesX;
+            this.frameTime = frameTime; 
+            frameTimeLeft = frameTime;
+            var frameWidth = texture.Width/framesX;
+            var frameHeight = texture.Height;
+            for(int i =0 ; i < frames; i++)
+            {
+                _sourceRectangles.Add(new Rectangle(i * frameWidth, 0, frameWidth, frameHeight));
+            }
+        }
+
+        public void Start()
+        {
+            active = true;
+        }
+
+        public void Stop()
+        {
+            active = false;
+        }
+
+        public void Reset()
+        {
+            frame = 0;
+            frameTimeLeft = frameTime;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!active)
+            {
+                return;
+            }
+
+            frameTimeLeft -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(frameTimeLeft <= 0)
+            {
+                frameTimeLeft += frameTime;
+                frame = (frame +1 ) % frames;
+            }
+        }
+
+        public void Draw(Vector2 pos, SpriteBatch s,float transparency)
+        {
+            s.Draw(texture, pos, _sourceRectangles[frame], Color.White * transparency, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 1);
+        }
+    }
+}
